72beb44b3923c5d3f57a20828e4ce51d08342c53
Belen Garcia <belen.garcia@MacBook-Pro-de-pc1177.local>
2018-07-21 13:39:14 +0200

    log

--- starter_code/controllers/auth.controller.js
+++ starter_code/controllers/auth.controller.js
@@ -12,25 +12,10 @@ module.exports.doSignup = (req, res, next) => {
     const nameInput = req.body.name;
     const emailInput = req.body.email;
     const passwordInput = req.body.password;
-  
-    if (emailInput === '' || passwordInput === '') {
-      res.render('auth/signup', {
-        errorMessage: 'Enter both email and password to sign up.'
-      });
-      return;
-    }
-  
-    User.findOne({ email: emailInput }, '_id', (err, existingUser) => {
-      if (err) {
-        next(err);
-        return;
-      }
-  
-      if (existingUser !== null) {
-        res.render('auth/signup', {
-          errorMessage: `The email ${emailInput} is already in use.`
-        });
-        return;
-        }
-      });
+
+    console.log(req.body)
+    User.findOne(emailInput)
+        .then(user =>{
+            
+        })
     };

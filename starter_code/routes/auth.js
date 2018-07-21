d2b26bee00542e20ec9a8e5fe82d617e106240c1
Belen Garcia <belen.garcia@MacBook-Pro-de-pc1177.local>
2018-07-21 12:38:15 +0200

    salt rounds fixed

--- starter_code/routes/auth.js
+++ starter_code/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require ("../models/user")
 
-const bcrypt = 10;
+const bcryptSalt = 10;
 
 router.get('/signup', (req, res, next) => {
     res.render('auth/signup', {

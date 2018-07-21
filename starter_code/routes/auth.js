1135198e4f4a390118a1f4da30bc44b5cee6b725
Belen Garcia <belen.garcia@MacBook-Pro-de-pc1177.local>
2018-07-21 12:27:17 +0200

    1st redirection

--- starter_code/routes/auth.js
+++ starter_code/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/signup', (requestAnimationFrame, res, next) => {
+router.get('/signup', (req, res, next) => {
     res.render('auth/signup', {
         errorMessage: ''
     })

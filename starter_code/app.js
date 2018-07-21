40e83d0cae5c7f0d23efd932783728023fe7739a
Belen Garcia <belen.garcia@MacBook-Pro-de-pc1177.local>
2018-07-21 12:54:11 +0200

    ue

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
 const index = require('./routes/index')
 const authRoutes = require('./routes/auth')
@@ -40,6 +42,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.
 
 
 // default value for title local
